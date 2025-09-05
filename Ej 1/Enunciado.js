5b7a3e6f5e734474d4fc339c0e3a5caee636eba9
kipnap <thekipnap@gmail.com>
2025-09-05 19:51:16 +0200

    Primer ejercicio realizado satisfactoriamente

---
+++ Ej 1/Enunciado.js
@@ -0,0 +1,61 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal.
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 1:
+Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
+mostrar la información de un superheroe.
+ 
+Y añade una capacidad de buscar la información de varios superhéroes a la vez.
+ 
+Ejemplo:
+mostrarInformacionSuperheroe('Iron Man');
+ 
+Salida:
+Nombre real: Tony Stark
+Poderes: Tecnología avanzada, Movilidad aérea
+Equipo: Los vengadores
+ 
+Ejemplo 2: 
+mostrarInformacionSuperheroes([array de nombres]]);
+ 
+Salida:
+Muestra la información de todos los superheroes
+ 
+*/
+ 
+// Crear array de objetos de superheroes marvel
+const infoSuperheroes = {
+    'Iron Man': {
+        nombreReal: 'Tony Stark',
+        poderes: ['Tecnologia avanzada', 'Movilidad aérea'],
+        equipo: 'Los vengadores'
+    },
+    'Capitán América': {
+        nombreReal: 'Steve Rogers',
+        poderes: ['Fuerza sobrehumana', 'Agilidad y reflejos sobresalientes'],
+        equipo: 'Los vengadores'
+    },
+    'Thor': {
+        nombreReal: 'Thor Odinson',
+        poderes: ['Mjolnir', 'Viento y trueno'],
+        equipo: 'Los vengadores'
+    },
+    'Spider-Man': {
+        nombreReal: 'Peter Parker',
+        poderes: ['Balanceo', 'Telarañas pegajosas', 'Sentido aracnido'],
+        equipo: 'Los vengadores'
+    },
+    'Hulk': {
+        nombreReal: 'Bruce Banner',
+        poderes: ['Fuerza sobrehumana', 'Invulnerabilidad'],
+        equipo: 'Los vengadores'
+    }
+};
+
+module.exports = infoSuperheroes;
---
+++ Ej 1/main.js
@@ -0,0 +1,25 @@
+import infoSuperheroes from "./Enunciado.js";
+
+
+
+function mostrarInformacionSuperheroes(superheroes){
+    
+    const lista = (Array.isArray(superheroes)) ?superheroes :[superheroes];
+
+    lista.forEach((nombre) => {
+        if(infoSuperheroes[nombre]){
+
+            console.log(`${nombre}\n`);
+            console.log(`Nombre real: ${infoSuperheroes[nombre].nombreReal}`);
+            console.log(`Poderes: ${infoSuperheroes[nombre].poderes}`);
+            console.log(`Equipo: ${infoSuperheroes[nombre].equipo}\n`);
+        }else{
+            console.log(`El superheroe ${nombre} no existe\n`);
+        }
+    });
+}
+
+mostrarInformacionSuperheroes(['Iron Man','Thor','Hulk','La cosa'])
+
+
+
